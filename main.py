dfd9c039364d614394f5d77416b8ff6fe6a2be5f
Baastheglass <lightningblazer123@gmail.com>
2025-03-17 16:33:23 +0500

    1/4 done

--- main.py
+++ main.py
@@ -25,11 +25,54 @@ correlation_matrix = df.corr()
 plt.figure(figsize=(24, 12))  # Adjust the figure size as needed
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')  # You can change 'coolwarm' to other colormaps
 plt.title('Correlation Heatmap of Features')
-plt.show()
+# plt.show()
+#features are too many making visualising relationships difficult
 
 #finding relevant features for feature selection in terms of Brute Force Web Attacks
 target_correlation = correlation_matrix[' Label_Web Attack � Brute Force'].sort_values(ascending=False)
 print("\nCorrelation with Brute Force Web Attacks:\n", target_correlation)
-relevant_features = correlation_matrix[target_correlation > 0]
+brute_relevant_features = target_correlation[target_correlation > 0]
+brute_relevant_features = brute_relevant_features.index.tolist()
+brute_positive_df = df[brute_relevant_features]
+brute_positive_df[' Label_Web Attack � Brute Force'] = df[' Label_Web Attack � Brute Force']
 
+#splitting data into adequate portions
+train_data, temp_data = train_test_split(brute_positive_df, test_size=0.3)
+val_data, test_data = train_test_split(temp_data, test_size=0.5)
 
+#splitting training data into x and y values
+x_train = train_data.drop(columns=[' Label_Web Attack � Brute Force'])
+y_train = train_data[' Label_Web Attack � Brute Force']
+
+x_val = val_data.drop(columns=[' Label_Web Attack � Brute Force'])
+y_val = val_data[' Label_Web Attack � Brute Force']
+
+#training model
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_val)
+
+accuracy = accuracy_score(y_val, y_pred)
+report = classification_report(y_val, y_pred)
+
+print("Model Accuracy for Brute Force Web Attacks: ", accuracy)
+print("Classification Report for Brute Force Web Attacks", report)
+
+cm = confusion_matrix(y_val, y_pred)
+plt.figure()
+sns.heatmap(cm, cmap='coolwarm', annot=True, fmt='d')
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.show()
+
+#ROC_Curve
+fpr, tpr, thresholds = roc_curve(y_val, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend()
+plt.show()
